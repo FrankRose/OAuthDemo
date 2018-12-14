da2d530b4aa01b524bdaac2a424ff6a8adb930ce
Frank Rose <franklin.rose@gmail.com>
2018-12-14 13:50:14 -0500

    Completed oauth router

--- server/oauth.js
+++ server/oauth.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
 const passport = require('passport');
 module.exports = router;
+
+router.get('/', passport.authenticate('google', { scope: 'email' }));
+
+router.get(
+  '/callback',
+  passport.authenticate('google', {
+    successRedirect: '/home',
+    failureRedirect: '/',
+  })
+);
